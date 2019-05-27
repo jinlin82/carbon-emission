4a7303384814ee54572f8cf2cf67b8bc4643cc8d
Wang HongHONG <18434365732@163.COM>
2019-05-27 19:01:52 +0800

    MODIFY CODE

--- cross.R
+++ cross.R
@@ -1,3 +1,13 @@
+#history2=read.csv("E:/github_repo/carbon-emission/data/updata/guangdong2.csv")
+#colnames(history2)=c("ds","y","weekday")
+#history2$ds=as.Date(history2$ds,"%Y/%m/%d")
+#library(prophet)
+#history=history2[,c(1,2)]
+#m <- prophet(weekly.seasonality=FALSE, changepoint.prior.scale = 0.05)
+#m <- add_seasonality(m, name='weaking', period=5, fourier.order=3)
+#m <- fit.prophet(m, history)
+#crossday=cross_validation(m,90,units="days")#运行时间略长
+#perform=performance_metrics(crossday,rolling_window = 0.1)
 #############arima和ets的cross对象#################
 rm(list=ls())
 ####1切割点产生################
@@ -34,9 +44,9 @@ generate_cutoffs <- function(df, horizon, initial, period) {
   ))
   return(rev(result))
 }
-###################函数#########################################
+###################求arima的cross#########################################
 cross_validation2 <- function(
-  a,model, horizon, units, period = NULL, initial = NULL) {
+  model, horizon, units, period = NULL, initial = NULL) {
   df=model$history
   horizon.dt <- as.difftime(horizon, units = "days")
   initial.dt <- as.difftime(3 * horizon, units = "days")
@@ -47,13 +57,32 @@ cross_validation2 <- function(
     cutoff <- cutoffs[i]
     history.c <- dplyr::filter(df, ds <= cutoff)#产生拟合数据
     df.predict <- dplyr::filter(df, ds > cutoff, ds <= cutoff + horizon.dt)#真实值
-    if("a"=="arima"){#函数不能直接比较
-      m2<- a(history.c$y,order=c(1,1,1))#模型拟合
-    }else{
-      m2=a(history.c$y)
-    }
-    yhat <- forecast(m2, length(df.predict[,1]))#预测值
-    df.c <- cbind(df.predict, yhat)[,c(1,2,6,9,10)]
+    m2<- arima(history.c$y,order=c(1,1,1))#模型拟合
+    yhat <- forecast::forecast(m2, length(df.predict[,1]))#预测值
+    df.c <- cbind(df.predict, yhat)[,c("ds","y","Point Forecast","Lo 95","Hi 95")]
+    colnames(df.c)=c("ds",'y','yhat','yhat_lower', 'yhat_upper')#选用95%置信区间
+    df.c$cutoff <- cutoff# Merge yhat, y, and cutoff.
+    predicts <- rbind(predicts, df.c)
+  }
+  return(predicts)
+}
+###################求ets的cross#########################################
+#i=2;model=m;horizon=90
+cross_validation3 <- function(
+  model, horizon, units, period = NULL, initial = NULL) {
+  df=model$history
+  horizon.dt <- as.difftime(horizon, units = "days")
+  initial.dt <- as.difftime(3 * horizon, units = "days")
+  period.dt <- as.difftime(0.5 * horizon, units ="days")
+  cutoffs <- generate_cutoffs(df, horizon.dt, initial.dt, period.dt)
+  predicts <- data.frame()
+  for (i in 1:length(cutoffs)) {
+    cutoff <- cutoffs[i]
+    history.c <- dplyr::filter(df, ds <= cutoff)#产生拟合数据
+    df.predict <- dplyr::filter(df, ds > cutoff, ds <= cutoff + horizon.dt)#真实值
+    m2<- ets(history.c$y)#模型拟合
+    yhat <- forecast::forecast(m2, length(df.predict[,1]))#预测值
+    df.c <- cbind(df.predict, yhat)[,c("ds","y","Point Forecast","Lo 95","Hi 95")]
     colnames(df.c)=c("ds",'y','yhat','yhat_lower', 'yhat_upper')#选用95%置信区间
     df.c$cutoff <- cutoff# Merge yhat, y, and cutoff.
     predicts <- rbind(predicts, df.c)
@@ -61,8 +90,9 @@ cross_validation2 <- function(
   return(predicts)
 }
 
-arimacrossday=cross_validation2(arima,m,horizon = 90,initial = 270,period = 45)
-etscrossday=cross_validation2(ets,m,horizon = 90,initial = 270,period = 45)
+arimacrossday=cross_validation2(m,horizon = 90,initial = 270,period = 45)
+
+etscrossday=cross_validation3(m,horizon = 90,initial = 270,period = 45)
 
 arimaperform=performance_metrics(arimacrossday,rolling_window = 0.1)
 etsperform=performance_metrics(etscrossday,rolling_window = 0.1)
@@ -77,7 +107,7 @@ rolling_mean_by_h <- function(x, h, w, name) {
   # Aggregate over h
   df <- data.frame(x=x, h=h)
   df2 <- df %>%
-    dplyr::group_by(h) %>%
+    dplyr::group_by(h) %>% 
     dplyr::summarise(mean = mean(x), n = dplyr::n())
   
   xm <- df2$mean
@@ -113,21 +143,21 @@ rolling_mean_by_h <- function(x, h, w, name) {
   return(res)
 }
 
-
-mape <- function(df, w) {
-  ape <- abs((df$y - df$yhat) / df$y)
+#df=crossday;w=66;x=ape;h=df$horizon;name="mape"
+mape2 <- function(df, w) {
   df$horizon <- df$ds - df$cutoff
   df <- df[order(df$horizon),]
+  ape <- abs((df$y - df$yhat) / df$y)
   if (w < 0) {
     return(data.frame(horizon = df$horizon, mape = ape))
   }
   return(rolling_mean_by_h(x = ape, h = df$horizon, w = w, name = 'mape'))
 }
 
-arimamape=mape(arimacrossday,66)
-prophetmape=mape(crossday,66)
-etsmape=mape(etscrossday,66)
+arimamape=mape2(arimacrossday,66)
+prophetmape=mape2(crossday,66)
+etsmape=mape2(etscrossday,66)
 plot(etsmape,type="l",xlab="Horizon(days)",ylab="mape",ylim=c(0,0.2),lty=1)
-lines(prophetmape,type="l",lty=2)
+lines(prophetmape,type="p",lty=2)
 lines(arimamape,type="l",lty=3)
-legend("topright",c("arimamape","prophetmape","etsmape"),cex=0.7,lty=c(1,2,3),x.intersp=0.25,y.intersp=0.5)
+legend("topright",c("etsmape","prophetmape","arimamape"),cex=0.7,lty=c(1,2,3),x.intersp=0.25,y.intersp=0.5)
